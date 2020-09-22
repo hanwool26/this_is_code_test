0a4bebd67df6882be5238375cbb8404812bf5b1d
Hanwool Lee <hanwool26@gmail.com>
2020-09-22 23:06:32 +0900

    this is code test#3

--- how_to_solve_algo.py
+++ how_to_solve_algo.py
@@ -8,3 +8,43 @@ print(a[1:3])
 # List comprehension
 array = [i for i in range(10) if i % 2 == 0]
 print(array)
+
+# 2차원 리스트 초기화
+n = 4
+m = 3
+
+array = [[0] * m for _ in range(n)] # 내부적으로 parameter를 쓰지 않을 때 _ 를 씀
+print(array)
+
+# 자주쓰는 method
+# append() / sort() / reverse() / insert() / count() / remove()
+
+# sort와 sorted의 차이점은 sorted() 는 객체를 복사하여 기존값을 변형시키지않음.
+
+# 문자열 자료형
+# 문자열 변수를 초기화할 떄는 "", '' 를 사용
+
+# Turple
+# - 한번 선언된 값을 변경할 수 없다
+# - ()로 구성
+
+a = (1,2,3,4)
+print(a)
+#a[2] =7 # error because a is turple
+
+data = dict()
+data['사과'] = 'Apple'
+data['바나나'] = 'Banana'
+data['코코넛'] = 'Coconut'
+
+print(data)
+print(data.keys())
+print(data.values())
+
+# 집합 자료형
+# - 중복을 허용하지 않음
+# - 순서가 없음
+
+data = set([1,1,2,3,4,4,5])
+print(data)
+
